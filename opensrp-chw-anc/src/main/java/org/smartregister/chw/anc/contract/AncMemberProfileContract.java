5c3a3c000850be965eab4195bc1fe1824a9b5871
Manu Tarus <manutarus@gmail.com>
2019-05-30 17:12:49 +0300

    add anc member contract

--- opensrp-chw-anc/src/main/java/org/smartregister/chw/anc/contract/AncMemberProfileContract.java
+++ opensrp-chw-anc/src/main/java/org/smartregister/chw/anc/contract/AncMemberProfileContract.java
@@ -2,103 +2,36 @@ package org.smartregister.chw.anc.contract;
 
 import android.content.Context;
 
-import org.json.JSONObject;
-import org.smartregister.commonregistry.CommonPersonObjectClient;
-import org.smartregister.domain.FetchStatus;
+import org.smartregister.chw.anc.util.MemberObject;
 import org.smartregister.view.contract.BaseProfileContract;
 
 public interface AncMemberProfileContract {
 
-    interface View extends BaseProfileContract.View {
-
-        Context getApplicationContext();
-
+    interface View {
         Context getContext();
 
-        String getString(int resourceId);
-
-        void startFormActivity(JSONObject form);
-
-        void refreshProfile(final FetchStatus fetchStatus);
-
-        void displayShortToast(int resourceId);
-
-        void setProfileImage(String baseEntityId);
-
-        void setParentName(String parentName);
-
-        void setGender(String gender);
-
-        void setAddress(String address);
-
-        void setId(String id);
-
-        void setProfileName(String fullName);
-
-        void setAge(String age);
-
-        void setVisitButtonDueStatus();
-
-        void setVisitButtonOverdueStatus();
-
-        void setVisitNotDoneThisMonth();
-
-        void setLastVisitRowView(String days);
-
-        void setServiceNameDue(String name, String dueDate);
-
-        void setServiceNameOverDue(String name, String dueDate);
-
-        void setServiceNameUpcoming(String name, String dueDate);
-
-        void setVisitLessTwentyFourView(String monthName);
-
-        void setVisitAboveTwentyFourView();
-
-        void setFamilyHasNothingDue();
-
-        void setFamilyHasServiceDue();
-
-        void setFamilyHasServiceOverdue();
+        void setMemberName(String memberName);
 
         AncMemberProfileContract.Presenter presenter();
 
-        void updateHasPhone(boolean hasPhone);
-
-        void enableEdit(boolean enable);
-        void hideProgressBar();
     }
 
     interface Presenter extends BaseProfileContract.Presenter {
-
 
         AncMemberProfileContract.View getView();
 
         void fetchProfileData();
 
-        void updateChildCommonPerson(String baseEntityId);
-
-        void updateVisitNotDone(long value);
-
-        void fetchVisitStatus(String baseEntityId);
-
-        void fetchUpcomingServiceAndFamilyDue(String baseEntityId);
-
     }
 
     interface Interactor {
 
-        void onDestroy(boolean isChangingConfiguration);
-
-        void updateChildCommonPerson(String baseEntityId);
-
+        void refreshProfileView(MemberObject memberObject, boolean isForEdit, AncMemberProfileContract.InteractorCallBack callback);
     }
 
+    interface InteractorCallBack {
 
-
-    interface Model {
-
-        JSONObject getFormAsJson(String formName, String entityId, String currentLocationId, String familyID) throws Exception;
+        void refreshProfileTopSection(MemberObject memberObject);
 
     }
 
