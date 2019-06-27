a3453fd5073c8821d640c1b26cc73951c4c10ead
rkodev <43806892+rkodev@users.noreply.github.com>
2019-06-27 23:31:30 +0300

    Refactored home visit action

--- opensrp-chw-anc/src/main/java/org/smartregister/chw/anc/model/BaseHomeVisitHistoricAction.java
+++ opensrp-chw-anc/src/main/java/org/smartregister/chw/anc/model/BaseHomeVisitHistoricAction.java
@@ -1,4 +1,33 @@
 package org.smartregister.chw.anc.model;
 
+import java.util.List;
+
 public class BaseHomeVisitHistoricAction {
+
+    private String Title = "";
+    private List<String> details = null;
+
+    public BaseHomeVisitHistoricAction(String title, List<String> details) {
+        Title = title;
+        this.details = details;
+    }
+
+    public BaseHomeVisitHistoricAction() {
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public List<String> getDetails() {
+        return details;
+    }
+
+    public void setDetails(List<String> details) {
+        this.details = details;
+    }
 }
