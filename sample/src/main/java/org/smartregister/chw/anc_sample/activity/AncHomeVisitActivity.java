16c6a0776157dcc8d25f1df40251bf16174ccc35
rkodev <43806892+rkodev@users.noreply.github.com>
2019-07-03 10:18:06 +0300

    Updated sample app

--- sample/src/main/java/org/smartregister/chw/anc_sample/activity/AncHomeVisitActivity.java
+++ sample/src/main/java/org/smartregister/chw/anc_sample/activity/AncHomeVisitActivity.java
@@ -8,13 +8,15 @@ import org.smartregister.chw.anc.domain.MemberObject;
 import org.smartregister.chw.anc.presenter.BaseAncHomeVisitPresenter;
 import org.smartregister.chw.anc_sample.interactor.AncHomeVisitInteractor;
 
+import static org.smartregister.chw.anc.util.Constants.ANC_MEMBER_OBJECTS.EDIT_MODE;
 import static org.smartregister.chw.anc.util.Constants.ANC_MEMBER_OBJECTS.MEMBER_PROFILE_OBJECT;
 
 public class AncHomeVisitActivity extends BaseAncHomeVisitActivity {
 
-    public static void startMe(Activity activity, MemberObject memberObject) {
+    public static void startMe(Activity activity, MemberObject memberObject, Boolean isEditMode) {
         Intent intent = new Intent(activity, AncHomeVisitActivity.class);
         intent.putExtra(MEMBER_PROFILE_OBJECT, memberObject);
+        intent.putExtra(EDIT_MODE, isEditMode);
         activity.startActivity(intent);
     }
 
