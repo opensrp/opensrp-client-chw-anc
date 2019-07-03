dbc0831cddbb4281b1542f786e5cbfe2ed21d428
rkodev <43806892+rkodev@users.noreply.github.com>
2019-07-03 17:44:45 +0300

    Visit Details refactor

---
+++ opensrp-chw-anc/src/main/java/org/smartregister/chw/anc/util/VisitUtils.java
@@ -0,0 +1,44 @@
+package org.smartregister.chw.anc.util;
+
+import org.smartregister.chw.anc.AncLibrary;
+import org.smartregister.chw.anc.domain.Visit;
+import org.smartregister.chw.anc.domain.VisitDetail;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class VisitUtils {
+
+    public static List<Visit> getVisits(String memberID) {
+
+        List<Visit> visits = new ArrayList<>(AncLibrary.getInstance().visitRepository().getVisits(memberID, Constants.EVENT_TYPE.ANC_HOME_VISIT));
+
+        int x = 0;
+        while (visits.size() > x) {
+            Visit visit = visits.get(x);
+            List<VisitDetail> detailList = AncLibrary.getInstance().visitDetailsRepository().getVisits(visit.getVisitId());
+            visits.get(x).setVisitDetails(getVisitGroups(detailList));
+            x++;
+        }
+
+        return visits;
+    }
+
+    private static Map<String, List<VisitDetail>> getVisitGroups(List<VisitDetail> detailList) {
+        Map<String, List<VisitDetail>> visitMap = new HashMap<>();
+
+        for (VisitDetail visitDetail : detailList) {
+
+            List<VisitDetail> visitDetailList = visitMap.get(visitDetail.getVisitKey());
+            if (visitDetailList == null)
+                visitDetailList = new ArrayList<>();
+
+            visitDetailList.add(visitDetail);
+
+            visitMap.put(visitDetail.getVisitKey(), visitDetailList);
+        }
+        return visitMap;
+    }
+}
