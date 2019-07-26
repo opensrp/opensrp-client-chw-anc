f8e5f92c03640ed777e2a3dfe2733b647e28c09c
rkodev <43806892+rkodev@users.noreply.github.com>
2019-07-26 17:06:03 +0300

    Updated sample app design

--- sample/src/main/java/org/smartregister/chw/anc_sample/activity/AncRegisterActivity.java
+++ sample/src/main/java/org/smartregister/chw/anc_sample/activity/AncRegisterActivity.java
@@ -12,16 +12,10 @@ import com.vijay.jsonwizard.domain.Form;
 import org.json.JSONObject;
 import org.smartregister.chw.anc.activity.BaseAncMemberProfileActivity;
 import org.smartregister.chw.anc.activity.BaseAncRegisterActivity;
-import org.smartregister.chw.anc.domain.MemberObject;
-import org.smartregister.chw.anc.util.DBConstants;
 import org.smartregister.chw.anc_sample.R;
 import org.smartregister.chw.anc_sample.fragment.AncRegisterFragment;
 import org.smartregister.chw.anc_sample.utils.JsonFormUtils;
-import org.smartregister.commonregistry.CommonPersonObjectClient;
 import org.smartregister.view.fragment.BaseRegisterFragment;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class AncRegisterActivity extends BaseAncRegisterActivity {
     @Override
@@ -67,47 +61,10 @@ public class AncRegisterActivity extends BaseAncRegisterActivity {
 
     private void openVisitPage() {
         // dumy common person object
-        Map<String, String> details = new HashMap<>();
-        details.put(DBConstants.KEY.FIRST_NAME, "Glory");
-        details.put(DBConstants.KEY.LAST_NAME, "Juma");
-        details.put(DBConstants.KEY.MIDDLE_NAME, "Wambui");
-        details.put(DBConstants.KEY.DOB, "1982-01-18T03:00:00.000+03:00");
-        details.put(DBConstants.KEY.LAST_HOME_VISIT, "");
-        details.put(DBConstants.KEY.VILLAGE_TOWN, "Lavingtone #221");
-        details.put(DBConstants.KEY.FAMILY_NAME, "Jumwa");
-        details.put(DBConstants.KEY.UNIQUE_ID, "3503504");
-        details.put(DBConstants.KEY.LAST_MENSTRUAL_PERIOD, "04-01-2019");
-        details.put(DBConstants.KEY.BASE_ENTITY_ID, "3503504");
-        details.put(DBConstants.KEY.FAMILY_HEAD, "3503504");
-        details.put(DBConstants.KEY.PRIMARY_CAREGIVER, "3503504");
-        details.put(DBConstants.KEY.PHONE_NUMBER, "0934567543");
-        CommonPersonObjectClient commonPersonObject = new CommonPersonObjectClient("", details, "Yo");
-        commonPersonObject.setColumnmaps(details);
-
-        MemberObject memberObject = new MemberObject(commonPersonObject);
-        AncHomeVisitActivity.startMe(this, memberObject, false);
+        AncHomeVisitActivity.startMe(this, EntryActivity.getSampleMember(), false);
     }
 
     private void openProfilePage() {
-
-        Map<String, String> details = new HashMap<>();
-        details.put(DBConstants.KEY.FIRST_NAME, "Glory");
-        details.put(DBConstants.KEY.LAST_NAME, "Juma");
-        details.put(DBConstants.KEY.MIDDLE_NAME, "Wambui");
-        details.put(DBConstants.KEY.DOB, "1982-01-18T03:00:00.000+03:00");
-        details.put(DBConstants.KEY.LAST_HOME_VISIT, "");
-        details.put(DBConstants.KEY.VILLAGE_TOWN, "Lavingtone #221");
-        details.put(DBConstants.KEY.FAMILY_NAME, "Jumwa");
-        details.put(DBConstants.KEY.UNIQUE_ID, "3503504");
-        details.put(DBConstants.KEY.LAST_MENSTRUAL_PERIOD, "04-01-2019");
-        details.put(DBConstants.KEY.BASE_ENTITY_ID, "3503504");
-        details.put(DBConstants.KEY.FAMILY_HEAD, "3503504");
-        details.put(DBConstants.KEY.PRIMARY_CAREGIVER, "3503504");
-        details.put(DBConstants.KEY.PHONE_NUMBER, "0934567543");
-        CommonPersonObjectClient commonPersonObject = new CommonPersonObjectClient("", details, "Yo");
-        commonPersonObject.setColumnmaps(details);
-
-        MemberObject memberObject = new MemberObject(commonPersonObject);
-        BaseAncMemberProfileActivity.startMe(this, memberObject,"Juma Family Head", "0976345634");
+        BaseAncMemberProfileActivity.startMe(this, EntryActivity.getSampleMember(), "Juma Family Head", "0976345634");
     }
 }
