6010e1c11d6f5ad7264943f9b40b0b226dcda2ab
Manu Tarus <manutarus@gmail.com>
2019-07-29 11:16:26 +0300

    add image view bordercolor

--- opensrp-chw-pnc/src/main/java/org/smartregister/chw/pnc/activity/BasePncMemberProfileActivity.java
+++ opensrp-chw-pnc/src/main/java/org/smartregister/chw/pnc/activity/BasePncMemberProfileActivity.java
@@ -34,6 +34,7 @@ public class BasePncMemberProfileActivity extends BaseAncMemberProfileActivity {
     @Override
     public void setMemberName(String memberName) {
         basePncMemberProfileInteractor.getPncMotherNameDetails(MEMBER_OBJECT, text_view_anc_member_name);
+//        imageView.setBorderColor(getResources().getColor(R.color.light_blue));
     }
 
 }
