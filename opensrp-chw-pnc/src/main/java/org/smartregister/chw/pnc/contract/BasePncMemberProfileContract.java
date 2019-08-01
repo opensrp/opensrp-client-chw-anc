276b128868a2d24bf9da66ba10837470ddeb33dd
Manu Tarus <manutarus@gmail.com>
2019-08-01 10:13:26 +0300

    save child updates

--- opensrp-chw-pnc/src/main/java/org/smartregister/chw/pnc/contract/BasePncMemberProfileContract.java
+++ opensrp-chw-pnc/src/main/java/org/smartregister/chw/pnc/contract/BasePncMemberProfileContract.java
@@ -1,9 +1,12 @@
 package org.smartregister.chw.pnc.contract;
 
+import android.util.Pair;
 import android.widget.TextView;
 
 import org.smartregister.chw.anc.contract.BaseAncMemberProfileContract;
 import org.smartregister.chw.anc.domain.MemberObject;
+import org.smartregister.clientandeventmodel.Client;
+import org.smartregister.clientandeventmodel.Event;
 import org.smartregister.commonregistry.CommonPersonObjectClient;
 
 import java.util.List;
@@ -18,6 +21,8 @@ public interface BasePncMemberProfileContract extends BaseAncMemberProfileContra
         String getPncDay(String motherBaseID);
 
         List<CommonPersonObjectClient> pncChildrenUnder29Days(String motherBaseID);
+
+        void updateChild(final Pair<Client, Event> pair, final String jsonString);
     }
 
 }
--- opensrp-chw-pnc/src/main/java/org/smartregister/chw/pnc/interactor/BasePncMemberProfileInteractor.java
+++ opensrp-chw-pnc/src/main/java/org/smartregister/chw/pnc/interactor/BasePncMemberProfileInteractor.java
@@ -1,5 +1,6 @@
 package org.smartregister.chw.pnc.interactor;
 
+import android.util.Pair;
 import android.widget.TextView;
 
 import org.apache.commons.lang3.StringUtils;
@@ -14,11 +15,14 @@ import org.smartregister.chw.pnc.PncLibrary;
 import org.smartregister.chw.pnc.R;
 import org.smartregister.chw.pnc.contract.BasePncMemberProfileContract;
 import org.smartregister.chw.pnc.util.Constants;
+import org.smartregister.clientandeventmodel.Client;
+import org.smartregister.clientandeventmodel.Event;
 import org.smartregister.commonregistry.CommonPersonObjectClient;
 
 import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
+import timber.log.Timber;
 
 import static org.smartregister.util.Utils.getName;
 
@@ -41,13 +45,10 @@ public class BasePncMemberProfileInteractor extends BaseAncMemberProfileInteract
         List<CommonPersonObjectClient> children = pncChildrenUnder29Days(memberObject.getBaseEntityId());
         String nameDetails = memberObject.getMemberName();
         textView.setText(nameDetails);
-        if (children.size() > 0) {
-            textView.setSingleLine(false);
-            char gender;
-            CommonPersonObjectClient childObject;
-            for (int i = 0; i < children.size(); i++) {
-                childObject = children.get(i);
-                gender = childObject.getColumnmaps().get(Constants.KEY.GENDER).charAt(0);
+        textView.setSingleLine(false);
+        for (CommonPersonObjectClient childObject : children) {
+            try {
+                char gender = childObject.getColumnmaps().get(Constants.KEY.GENDER).charAt(0);
                 textView.append(" +\n" + childNameDetails(childObject.getColumnmaps().get(DBConstants.KEY.FIRST_NAME),
                         childObject.getColumnmaps().get(DBConstants.KEY.MIDDLE_NAME),
                         childObject.getColumnmaps().get(DBConstants.KEY.LAST_NAME),
@@ -56,6 +57,8 @@ public class BasePncMemberProfileInteractor extends BaseAncMemberProfileInteract
                     imageView.setBorderColor(PncLibrary.getInstance().context().getColorResource(R.color.light_blue));
                 else
                     imageView.setBorderColor(PncLibrary.getInstance().context().getColorResource(R.color.light_pink));
+            } catch (NullPointerException npe) {
+                Timber.e(npe);
             }
         }
 
@@ -70,6 +73,7 @@ public class BasePncMemberProfileInteractor extends BaseAncMemberProfileInteract
     private String childNameDetails(String firstName, String middleName, String surName, String age, char gender) {
         String dayCountString = PncLibrary.getInstance().context().getStringResource(R.string.pnc_day_count);
         String spacer = ", ";
+        middleName = middleName != null ? middleName : "";
         String name = getName(firstName, middleName);
         name = getName(name, surName);
 
@@ -77,5 +81,9 @@ public class BasePncMemberProfileInteractor extends BaseAncMemberProfileInteract
             return name + spacer + age + dayCountString + spacer + gender;
         }
         return null;
+    }
+
+    public void updateChild(final Pair<Client, Event> pair, final String jsonString) {
+//        implement on the app
     }
 }
