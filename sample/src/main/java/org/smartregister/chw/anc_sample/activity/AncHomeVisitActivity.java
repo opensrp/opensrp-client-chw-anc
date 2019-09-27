4c13f393dbc935731827f013ad773a7dff738f33
rkodev <43806892+rkodev@users.noreply.github.com>
2019-09-27 11:56:05 +0300

    Updated anc home visit in sample activity

--- sample/src/main/java/org/smartregister/chw/anc_sample/activity/AncHomeVisitActivity.java
+++ sample/src/main/java/org/smartregister/chw/anc_sample/activity/AncHomeVisitActivity.java
@@ -11,7 +11,7 @@ import org.smartregister.chw.anc_sample.interactor.AncHomeVisitInteractor;
 public class AncHomeVisitActivity extends BaseAncHomeVisitActivity {
 
     public static void startMe(Activity activity, String baseEntityID, Boolean isEditMode) {
-        Intent intent = new Intent(activity, BaseAncHomeVisitActivity.class);
+        Intent intent = new Intent(activity, AncHomeVisitActivity.class);
         intent.putExtra(Constants.ANC_MEMBER_OBJECTS.BASE_ENTITY_ID, baseEntityID);
         intent.putExtra(Constants.ANC_MEMBER_OBJECTS.EDIT_MODE, isEditMode);
         activity.startActivityForResult(intent, Constants.REQUEST_CODE_HOME_VISIT);
