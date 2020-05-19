17b5cbd963f9e69b01e5cc96e1b5154b4d89480a
Manu Tarus <manutarus@gmail.com>
2020-05-19 21:26:07 +0300

    update codacy require changes

--- opensrp-chw-anc/src/main/java/org/smartregister/chw/anc/fragment/BaseAncRespondersCallDialogFragment.java
+++ opensrp-chw-anc/src/main/java/org/smartregister/chw/anc/fragment/BaseAncRespondersCallDialogFragment.java
@@ -1,20 +1,15 @@
 package org.smartregister.chw.anc.fragment;
 
 import android.app.Activity;
-import android.app.DialogFragment;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
-import android.content.Context;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.TextView;
 
 import org.apache.commons.lang3.StringUtils;
-import org.smartregister.chw.anc.contract.BaseAncWomanCallDialogContract;
 import org.smartregister.chw.anc.listener.BaseAncResponderCallWidgetDialogListener;
 import org.smartregister.chw.opensrp_chw_anc.R;
 
@@ -23,7 +18,8 @@ import static org.smartregister.util.Utils.getName;
 public class BaseAncRespondersCallDialogFragment extends BaseAncWomanCallDialogFragment {
 
     public static final String DIALOG_TAG = "BaseAncRespondersCallDialogFragment_DIALOG_TAG";
-    private static String ancResponderName, ancResponderPhoneNumber;
+    private static String ancResponderName;
+    private static String ancResponderPhoneNumber;
     private View.OnClickListener listener = null;
 
     public static BaseAncRespondersCallDialogFragment launchDialog(Activity activity, String responderName, String responderPhone) {
