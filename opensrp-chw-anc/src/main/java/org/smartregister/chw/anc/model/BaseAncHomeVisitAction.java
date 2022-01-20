82589909e62c7bebc53c220481aaf828201ec03e
Allan O <a.onchuru@gmail.com>
2022-01-20 23:30:39 +0300

    :fire: Implement BaseAncHomeVisitAction
    Extend the new HomeVisitAction class

---
+++ opensrp-chw-anc/src/main/java/org/smartregister/chw/anc/model/BaseAncHomeVisitAction.java
@@ -0,0 +1,31 @@
+package org.smartregister.chw.anc.model;
+
+import android.content.Context;
+
+public class BaseAncHomeVisitAction extends BaseHomeVisitAction {
+
+    protected BaseAncHomeVisitAction(Builder builder) throws ValidationException {
+        super(builder);
+    }
+
+    @Override
+    public String getSubTitle() {
+        return subTitle;
+    }
+
+    public static class Builder extends BaseHomeVisitAction.Builder<Builder> {
+
+        public Builder(Context context, String title) {
+            super(context, title);
+        }
+
+        @Override
+        public Builder getThis() {
+            return this;
+        }
+
+        public BaseAncHomeVisitAction build () throws ValidationException {
+            return new BaseAncHomeVisitAction(this);
+        }
+    }
+}
